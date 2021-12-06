16a824308b71028c55ba7023bb1ec91665f6e1ad
Jayesh Lalwani <jlalwani@amazon.com>
2021-12-06 11:33:08 -0500

    Made histogram more generic

--- src/adventofcode/year2021/day6/app/Main.hs
+++ src/adventofcode/year2021/day6/app/Main.hs
@@ -1,25 +1,38 @@
 module Main where
 
 import Debug.Trace (trace)
+import Data.Maybe (fromMaybe)
 import qualified Data.Map as Map
 
 input :: [Int]
 --input = [3,4,3,1,2]
 input=[5,1,1,1,3,5,1,1,1,1,5,3,1,1,3,1,1,1,4,1,1,1,1,1,2,4,3,4,1,5,3,4,1,1,5,1,2,1,1,2,1,1,2,1,1,4,2,3,2,1,4,1,1,4,2,1,4,5,5,1,1,1,1,1,2,1,1,1,2,1,5,5,1,1,4,4,5,1,1,1,3,1,5,1,2,1,5,1,4,1,3,2,4,2,1,1,4,1,1,1,1,4,1,1,1,1,1,3,5,4,1,1,3,1,1,1,2,1,1,1,1,5,1,1,1,4,1,4,1,1,1,1,1,2,1,1,5,1,2,1,1,2,1,1,2,4,1,1,5,1,3,4,1,2,4,1,1,1,1,1,4,1,1,4,2,2,1,5,1,4,1,1,5,1,1,5,5,1,1,1,1,1,5,2,1,3,3,1,1,1,3,2,4,5,1,2,1,5,1,4,1,5,1,1,1,1,1,1,4,3,1,1,3,3,1,4,5,1,1,4,1,4,3,4,1,1,1,2,2,1,2,5,1,1,3,5,2,1,1,1,1,1,1,1,4,4,1,5,4,1,1,1,1,1,2,1,2,1,5,1,1,3,1,1,1,1,1,1,1,1,1,1,2,1,3,1,5,3,3,1,1,2,4,4,1,1,2,1,1,3,1,1,1,1,2,3,4,1,1,2]
 
+-- --------- Generic Histogram
+type Histogram k = Map.Map k Integer
+
+
+histogram ::  Ord k => [k] -> Histogram k
+histogram as = foldl (updateHistogram(1)) Map.empty as
+
+updateHistogram :: Ord k => Integer -> Histogram k -> k -> Histogram k
+updateHistogram n acc a = Map.insertWith(+) a n acc
+
+
+count :: Ord k => k -> Histogram k -> Integer
+count a m = Map.findWithDefault 0 a m
+
+total :: Ord k => Histogram k -> Integer
+total m = Map.foldl (+) 0 m 
+
+-- ----------END generic histogram
+
 
 -- fishes are represented as a histogram . key is age of fish, value is number of fish
-type Fishes = Map.Map Int Integer 
+type Fishes = Histogram Int 
 
 
--- --------- Generic Histogram
-updateHistogram :: Ord a => Map.Map a Integer -> a -> Map.Map a Integer
-updateHistogram acc a = Map.insertWith(+) a 1 acc
 
-histogram ::  Ord a => [a] -> Map.Map a Integer
-histogram as = foldl updateHistogram Map.empty as
-
--- ----------END generic hoistogram
 
 toFishes :: [Int] -> Fishes
 toFishes inp = histogram inp 
@@ -46,7 +59,7 @@ doWhile acc doFunc whileFunc
 run :: Fishes -> Int -> IO ()
 run fishes n = do
     let (fishesN, _) = doWhile (fishes,n) nextGen isDone
-    let totalFishesN = Map.foldl (+) 0 fishesN 
+    let totalFishesN = total fishesN 
     putStrLn (show(n) ++ ":" ++ show(fishesN) ++ " - " ++ show(totalFishesN))
 
 main :: IO ()
