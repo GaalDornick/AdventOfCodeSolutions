09f85e8b41fb2d083416f2d157fde7aa2bbdc51c
Jayesh Lalwani <jlalwani@amazon.com>
2021-12-07 12:32:54 -0500

    Day7

--- src/adventofcode/year2021/day6/app/Main.hs
+++ src/adventofcode/year2021/day6/app/Main.hs
@@ -9,6 +9,8 @@ input :: [Int]
 input=[5,1,1,1,3,5,1,1,1,1,5,3,1,1,3,1,1,1,4,1,1,1,1,1,2,4,3,4,1,5,3,4,1,1,5,1,2,1,1,2,1,1,2,1,1,4,2,3,2,1,4,1,1,4,2,1,4,5,5,1,1,1,1,1,2,1,1,1,2,1,5,5,1,1,4,4,5,1,1,1,3,1,5,1,2,1,5,1,4,1,3,2,4,2,1,1,4,1,1,1,1,4,1,1,1,1,1,3,5,4,1,1,3,1,1,1,2,1,1,1,1,5,1,1,1,4,1,4,1,1,1,1,1,2,1,1,5,1,2,1,1,2,1,1,2,4,1,1,5,1,3,4,1,2,4,1,1,1,1,1,4,1,1,4,2,2,1,5,1,4,1,1,5,1,1,5,5,1,1,1,1,1,5,2,1,3,3,1,1,1,3,2,4,5,1,2,1,5,1,4,1,5,1,1,1,1,1,1,4,3,1,1,3,3,1,4,5,1,1,4,1,4,3,4,1,1,1,2,2,1,2,5,1,1,3,5,2,1,1,1,1,1,1,1,4,4,1,5,4,1,1,1,1,1,2,1,2,1,5,1,1,3,1,1,1,1,1,1,1,1,1,1,2,1,3,1,5,3,3,1,1,2,4,4,1,1,2,1,1,3,1,1,1,1,2,3,4,1,1,2]
 
 -- --------- Generic Histogram
+
+--Histogram is a Map, key is the item and value is the number of time item has occurred
 type Histogram k = Map.Map k Integer
 
 
@@ -25,10 +27,11 @@ count a m = Map.findWithDefault 0 a m
 total :: Ord k => Histogram k -> Integer
 total m = Map.foldl (+) 0 m 
 
+
 -- ----------END generic histogram
 
 
--- fishes are represented as a histogram . key is age of fish, value is number of fish
+
 type Fishes = Histogram Int 
 
 
